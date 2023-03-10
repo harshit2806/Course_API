625aac167aef7df19c2483fb722d441a55dfbba2
Harshit <harshitsoni2806@gmail.com>
2023-03-10 19:22:00 +0530

    Initial Commit

---
+++ src/main/java/in/hsoni/Course_track/Dao/Course_Dao.java
@@ -0,0 +1,9 @@
+package in.hsoni.Course_track.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.hsoni.Course_track.entities.Courses;
+
+public interface Course_Dao extends JpaRepository<Courses, Long> {
+
+}
